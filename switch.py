77c08e467765e317135c46e39fd96504e7dc4d2e
EllangoK <ellangok@bxscience.edu>
2019-12-09 15:51:07 -0500

    Will the real git switch please stand up

--- switch.py
+++ switch.py
@@ -4,7 +4,7 @@ import sys, os, csv
 
 account_dict = {}
 
-if (len(sys.argv) < 2):
+if (len(sys.argv) < 3):
     print("Please enter first name before any flags")
     sys.exit()
 
@@ -19,14 +19,17 @@ with open('accounts.csv') as csv_file:
             account_dict[row[0]] = (row[1],row[2])
             account_dict[row[2]] = (row[1],row[2])
 
-if sys.argv[1] in account_dict.keys():
-    print("Global settings set using: " + str(account_dict[sys.argv[1]]))
-    os.system('git config --global user.email ' + account_dict[sys.argv[1]][0])
-    os.system('git config --global user.name ' + account_dict[sys.argv[1]][1])
-    if sys.argv[2] == "-m":
-        os.system('git commit -m ' + '"' + ' '.join(sys.argv[3:]) + '"')
-    else:
-        os.system('git commit')
+if sys.argv[2] in account_dict.keys():
+    print("Global settings set using: " + str(account_dict[sys.argv[2]]))
+    os.system('git config --global user.email ' + account_dict[sys.argv[2]][0])
+    os.system('git config --global user.name ' + account_dict[sys.argv[2]][1])
+    if sys.argv[1] == "commit":
+        try:
+            if sys.argv[3] == "-m":
+                os.system('git commit -m ' + '"' + ' '.join(sys.argv[4:]) + '"')
+        except IndexError:
+            os.system('git commit')
+
 else:
     print("Please enter first name before any flags or message")
     sys.exit()
